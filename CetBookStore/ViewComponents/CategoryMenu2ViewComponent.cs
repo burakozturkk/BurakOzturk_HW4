da57ae6c00d38d149f4bac11276731aabd144d94
burak öztürk <brkozt94@gmail.com>
2020-05-03 23:34:39 +0300

    Cet322-Homework4

---
+++ CetBookStore/ViewComponents/CategoryMenu2ViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using CetBookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewComponents
+{
+    public class CategoryMenu2ViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoryMenu2ViewComponent(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cats = await context.Categories.ToListAsync();
+            return View(cats);
+        }
+    }
+}
